f937ff5e7c2e6aeeac6a5aaa81c5fb096c5385d3
restuarachman <57408456+restuarachman@users.noreply.github.com>
2019-11-09 19:32:13 +0700

    Menambahkan Func loadData
    belum bisa menginputkan nama lengkap

--- main.go
+++ main.go
@@ -6,24 +6,35 @@ import (
 	"os/exec"
 )
 
-type Namalengkap struct {
-	lengkap [6]string
-}
 type Mahasiswa struct {
-	nama   Namalengkap
+	nama   string
 	nim    string
 	matkul [10]Matakuliah
 }
+
+type arrMahasiswa struct {
+	data [1000]Mahasiswa
+	num  int
+}
+
 type Matakuliah struct {
 	nama  string
-	nilai int
+	nilai [3]int
 	sks   int
 }
 
+var (
+	mhs arrMahasiswa
+)
+
 func main() {
-	var pil, sure string
+	var (
+		pil, sure, paused string
+	)
 	clearScreen()
 
+	// jumlah mahasiswa
+	mhs.num = 0
 	fmt.Println("Selamat Datang Di Aplikasi Nilai Mahasiswa")
 	menu()
 	fmt.Print("Pilihan : ")
@@ -31,7 +42,8 @@ func main() {
 	clearScreen()
 	for pil != "0" {
 		if pil == "1" {
-
+			// input identitas mahasiswa
+			loadData(&mhs)
 		} else if pil == "2" {
 
 		} else if pil == "3" {
@@ -45,15 +57,17 @@ func main() {
 		} else {
 			fmt.Println("Inputan tidak valid")
 		}
-		fmt.Scanln()
+		fmt.Println("press any button")
+		fmt.Scanln(&paused)
 		clearScreen()
 
 		menu()
 		fmt.Print("Pilihan : ")
 		fmt.Scanln(&pil)
 		clearScreen()
+
 		if pil == "0" {
-			fmt.Println("Anda yakin untuk keluar?[Y/N]: ")
+			fmt.Print("Anda yakin untuk keluar?[Y/N]: ")
 			fmt.Scanln(&sure)
 			if sure == "N" || sure == "n" {
 				menu()
@@ -80,4 +94,31 @@ func menu() {
 	fmt.Println("5. Lihat Semua Data Mahasiswa")
 	fmt.Println("6. Hapus Data Mahasiswa")
 	fmt.Println("0. Keluar")
+}
+
+//Fungsi untuk input data mahasiswa
+func loadData(mhs *arrMahasiswa) {
+	var n int
+
+	fmt.Print("Jumlah mahasiswa : ")
+	fmt.Scanln(&n)
+	for i := 0; i < n; i++ {
+		fmt.Print("Nama	: ")
+		fmt.Scanln(&mhs.data[i].nama)
+		fmt.Print("NIM	: ")
+		fmt.Scanln(&mhs.data[i].nim)
+		fmt.Println("saved")
+		fmt.Println()
+		mhs.num++
+	}
+
+}
+
+//Menampilkan data mahasiswa
+func showData(jumlahMhs int, dataMhs arrMahasiswa) {
+	for i := 0; i < jumlahMhs; i++ {
+		fmt.Printf("Mahasiswa ke %d : \n", i+1)
+		fmt.Println("Nama	:", dataMhs.data[i].nama)
+		fmt.Println("Nim	:", dataMhs.data[i].nim)
+	}
 }
